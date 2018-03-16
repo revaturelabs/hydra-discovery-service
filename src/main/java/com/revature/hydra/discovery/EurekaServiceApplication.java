4911d95547aae07478000a63151d7a485a80e706
Matheu DeWitt <msdewitt@bsu.edu>
2018-03-16 13:35:40 -0400

    Add hystrix dashboard to discovery service.

--- src/main/java/com/revature/hydra/discovery/EurekaServiceApplication.java
+++ src/main/java/com/revature/hydra/discovery/EurekaServiceApplication.java
@@ -3,8 +3,10 @@ package com.revature.hydra.discovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
 @EnableEurekaServer
 public class EurekaServiceApplication {
 
